0323cc0f1399852a9d37a37b463e3a53d05c0a4c
Justin Vanderhooft <justinvdhooft@gmail.com>
2021-01-20 11:12:35 -0500

    fixup!build

--- config.forge.js
+++ config.forge.js
@@ -1,6 +1,6 @@
 module.exports = {
   "packagerConfig": {
-    "ignore": "\\.git(ignore|modules)|node_modules/(.cache|.cli-ngcc)|.vscode|src|e2e|.editorconfig|.eslintrc.json|.npmrc|angular.json|angular.webpack.js|main.js.map|main.ts|tsconfig.json|tsconfig.serve.json"
+    "ignore": "\\.git(ignore|modules)|node_modules/(.cache|.cli-ngcc)|.vscode|e2e|.editorconfig|.eslintrc.json|.npmrc|angular.json|angular.webpack.js|main.js.map|main.ts|tsconfig.json|tsconfig.serve.json|.env"
   },
   "makers": [
     {
@@ -9,7 +9,7 @@ module.exports = {
         "name": "project_xenomorph",
         // "certificateFile": process.env['WINDOWS_PFX_FILE'],
         // "certificatePassword": process.env['WINDOWS_PFX_PASSWORD'],
-        "signWithParams": `/f ${process.env['WINDOWS_PFX_FILE']} /p ${process.env['WINDOWS_PFX_PASSWORD']} /tr http://timestamp.comodoca.com /td sha256 /fd sha256 /sha1`
+        "signWithParams": `/f ${process.env['WINDOWS_PFX_FILE']} /p ${process.env['WINDOWS_PFX_PASSWORD']} /tr http://timestamp.comodoca.com /td sha256`
       }
     },
     {
