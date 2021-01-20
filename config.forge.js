b3b02684432c67fbb693c1a4e29fd4418a7b5f3a
Justin Vanderhooft <justinvdhooft@gmail.com>
2021-01-20 09:23:10 -0500

    fixup! more stuff

--- config.forge.js
+++ config.forge.js
@@ -7,8 +7,9 @@ module.exports = {
       "name": "@electron-forge/maker-squirrel",
       "config": {
         "name": "project_xenomorph",
-        "certificateFile": process.env['WINDOWS_PFX_FILE'],
-        "certificatePassword": process.env['WINDOWS_PFX_PASSWORD']
+        // "certificateFile": process.env['WINDOWS_PFX_FILE'],
+        // "certificatePassword": process.env['WINDOWS_PFX_PASSWORD'],
+        "signWithParams": `/f ${process.env['WINDOWS_PFX_FILE']} /p ${process.env['WINDOWS_PFX_PASSWORD']} /tr http://timestamp.comodoca.com /td sha256 /fd sha256 /sha1`
       }
     },
     {
