474f4dad9d964294405c7fc272ad4b058e5a3236
Justin Vanderhooft <justinvdhooft@gmail.com>
2021-01-20 09:04:18 -0500

    fixup! more

--- config.forge.js
+++ config.forge.js
@@ -1,4 +1,7 @@
 module.exports = {
+  "packagerConfig": {
+    "ignore": "\\.git(ignore|modules)|node_modules/(.cache|.cli-ngcc)|.vscode|src|e2e|.editorconfig|.eslintrc.json|.npmrc|angular.json|angular.webpack.js|main.js.map|main.ts|tsconfig.json|tsconfig.serve.json"
+  },
   "makers": [
     {
       "name": "@electron-forge/maker-squirrel",
