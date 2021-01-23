f481df7c5d54dbb59893e5bdf0a10ca606440aeb
Justin Vanderhooft <justinvdhooft@gmail.com>
2021-01-23 10:44:07 -0500

    feat(ui): support some basic filtering

--- src/app/plex/plex.component.ts
+++ src/app/plex/plex.component.ts
@@ -1,19 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbTypeaheadConfig } from '@ng-bootstrap/ng-bootstrap';
+import { Observable } from 'rxjs';
+import { debounceTime, distinctUntilChanged, map } from 'rxjs/operators';
 
 import { MESSAGE_CHANNEL, PLATFORMS } from '../../../core/constants';
 import { PlatformCore, PlatformCoreMapping } from '../../../core/types';
-import { ElectronService } from '../core/services';
 
 @Component({
   selector: 'app-plex',
   templateUrl: './plex.component.html',
-  styleUrls: ['./plex.component.scss']
+  styleUrls: ['./plex.component.scss'],
+  providers: [NgbTypeaheadConfig] // add NgbTypeaheadConfig to the component providers
 })
+
 export class PlexComponent implements OnInit {
+
+  public model: any;
 
   pmsLibraryExists = undefined;
 
-  constructor(private electron: ElectronService) { }
+  constructor(private config: NgbTypeaheadConfig) { }
 
   downloaded = false;
   needsUpdate = false;
@@ -39,6 +45,42 @@ export class PlexComponent implements OnInit {
     $core.downloaded = false;
   }
 
+  select(): void {
+    if (this.model) {
+      this.platforms = PLATFORMS.filter(p => p.name === this.model || !!p.cores.some(c => c.filename === this.model));
+    } else {
+      this.platforms = PLATFORMS;
+    }
+  }
+
+  search = (text$: Observable<string>) =>
+    text$.pipe(
+      debounceTime(50),
+      distinctUntilChanged(),
+      map(term => this.filter(term.toLocaleLowerCase()))
+    );
+
+  filter(term: string): any[] {
+    const results = [];
+
+    let platformResults;
+    let coreResults;
+    if (term.length > 1) {
+      platformResults = this.platforms.filter(p => p.name.toLowerCase().startsWith(term)).map(p => p.name);
+      if (platformResults.length) {
+        results.push(...platformResults);
+      }
+      for (const platform of this.platforms) {
+        coreResults = platform.cores.filter(c => c.filename.toLowerCase().startsWith(term)).map(c => c.filename);
+        if (coreResults.length) {
+          results.push(...coreResults);
+        }
+      }
+    }
+
+    return results.splice(0, 10);
+  }
+
   ngOnInit(): void {
     this.activeIds = [];
     for (let i = 0; i < PLATFORMS.length; i++) {
@@ -46,8 +88,9 @@ export class PlexComponent implements OnInit {
     }
 
     window.api.electronIpcInvoke(MESSAGE_CHANNEL.pmsLibraryCheck).then((result: boolean) => {
+      this.config.showHint = true;
       this.pmsLibraryExists = result;
-      console.log(`pmsLibraryExists: ${result.toString()}`);
+
       window.api.electronIpcInvoke(MESSAGE_CHANNEL.coreCheck).then((results: PlatformCoreMapping[]) => {
         if (results.length) {
           for (const result of results) {
