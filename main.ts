d0fab5623af1c264c11e21ee6bb401e389264e88
Justin Vanderhooft <justinvdhooft@gmail.com>
2021-01-24 09:15:11 -0800

    chore(core): log uncaught errors

--- main.ts
+++ main.ts
@@ -14,6 +14,7 @@ import registerUpdates from './main/update';
 logger.transports.file.level = 'silly';
 
 const log = logger.scope('startup');
+logger.catchErrors();
 
 if (squirrelStartup) {
   log.silly('squirrel statup mode. quitting.');
