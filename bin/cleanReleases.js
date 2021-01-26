5cc158c89b00064e6921a6da665094962e125229
Justin Vanderhooft <justinvdhooft@gmail.com>
2021-01-26 18:52:46 -0500

    fixup! edit cleanup script

--- bin/cleanReleases.js
+++ bin/cleanReleases.js
@@ -1,8 +1,12 @@
 const fs = require('fs');
 const path = require('path');
 
+try {
 const releases = fs.readFileSync(path.resolve('./out/make/squirrel.windows/x64/RELEASES'), 'utf-8');
 
 const formatted = releases.replace(/(http:\/\/downloads\.coremanager\.app\/download\/\d*\.\d*\.\d*\/)/, '');
 
 fs.writeFileSync(path.resolve('./out/make/squirrel.windows/x64/RELEASES'), formatted, 'utf-8');
+} catch (e) {
+  console.errer('could not clean RELEASES (maybe mac?)', e);
+}
