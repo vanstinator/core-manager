44667e504a4d5f6d736ebe8e5844b8f5efb14717
Justin Vanderhooft <justinvdhooft@gmail.com>
2021-01-29 19:47:29 -0500

    fix(core): log successful path detection correctly

--- main/handlers/pmsLibraryCheck.ts
+++ main/handlers/pmsLibraryCheck.ts
@@ -9,7 +9,7 @@ export default async function pmsLibraryCheck(): Promise<boolean> {
 
   try {
     await fs.promises.access(PMS_LIBRARY_PATH(), fs.constants.F_OK);
-    log.warn(`unable to access ${PMS_LIBRARY_PATH()}`);
+    log.info(`found PMS path: ${PMS_LIBRARY_PATH()}`);
   } catch (e) {
     log.error(`error accessing ${PMS_LIBRARY_PATH()}`, e);
     return false;
