a82cef9f3b1c392b773ba1c08f25fc635ceaf82e
Justin Vanderhooft <justinvdhooft@gmail.com>
2021-02-01 22:45:27 -0500

    fix(build): properly escape regex

--- config.forge.js
+++ config.forge.js
@@ -2,8 +2,8 @@ module.exports = {
   packagerConfig: {
     asar: true,
     icon: "./icons/icon",
-    ignore: "\\.git(ignore|modules)|node_modules/(.cache|.cli-ngcc)|.vscode|e2e|.editorconfig|.eslintrc.json|.npmrc|angular.json|angular.webpack.js|main.js.map|main.ts|tsconfig.json|tsconfig.serve.json|.env|icons",
-      asar: true,
+    ignore: "\\.git(ignore|modules)|node_modules/(\\.cache|.cli-ngcc)|\\.vscode|e2e|\\.editorconfig|\\.eslintrc\\.json|\\.npmrc|angular\\.json|angular\\.webpack\\.js|main\\.js\\.map|main\\.ts|tsconfig\\.json|tsconfig\\.serve\\.json|\\.env|icons",
+      // asar: true,
       darwinDarkModeSupport: 'true',
       // icon: 'electron-app/resources/icon',
       name: 'Core Manager',
